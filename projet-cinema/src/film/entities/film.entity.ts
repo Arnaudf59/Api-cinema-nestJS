50e90744cf42e3dfc025f3867bd41edb19646ce0
Arnaud <84374194+Arnaudf59@users.noreply.github.com>
2021-09-01 11:05:56 +0200

    champs unique bdd 1

--- projet-cinema/src/film/entities/film.entity.ts
+++ projet-cinema/src/film/entities/film.entity.ts
@@ -1,7 +1,8 @@
 import { SeanceEntity } from "src/seance/entities/seance.entity";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 @Entity('films')
+@Unique(["nom"])
 export class FilmEntity {
 
     @PrimaryGeneratedColumn({name : 'film_id'})
